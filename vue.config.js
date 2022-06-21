ab978d12fbb46b0c237045af321ad3476160393d
x <songbai_x@qq.com>
2022-06-21 10:01:38 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   productionSourceMap: false,
-  publicPath: process.env.NODE_ENV == 'production' ? "/py-tk" : "/"
+  publicPath: process.env.NODE_ENV == 'production' ? "/tkinter-helper" : "/"
 })
