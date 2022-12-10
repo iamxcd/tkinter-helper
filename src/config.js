1baa8748faa585cb2723e1ea4ba3150f1a3a4d75
x <songbai_x@qq.com>
2022-12-10 21:04:13 +0800

    更新版本

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const preview = {
     url: 'http://127.0.0.1:12300',
-    version: '2.1.2',
+    version: '2.2.0',
 }
 
 export const baseURL = 'https://v1.pytk.net/api';
@@ -10,6 +10,6 @@ export default {
     web: 'https://pytk.net/tkinter-helper',
     github: 'https://github.com/iamxcd/tkinter-helper',
     gitee: 'https://gitee.com/iamxcd/tkinter-helper',
-    version: '2.4.2',
+    version: '2.5.0',
     qq_group: '788392508'
 }
